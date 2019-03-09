bdd3984728bf94c5c426f6fd0553866b56d6182a
Miguel Victor <miguelvictor.remulta@outlook.com>
2019-03-09 17:53:50 +0800

    Added release configs

--- android/app/src/main/java/com/example/chinese_words/MainActivity.java
+++ android/app/src/main/java/com/example/chinese_words/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.chinese_words;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -8,6 +12,9 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    AppCenter.start(getApplication(), "eaee694a-dc40-4073-a33b-75c84a2342ab", Analytics.class, Crashes.class);
+
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
